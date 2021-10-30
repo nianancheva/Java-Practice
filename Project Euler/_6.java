1436f6be7311756abf19d4cf1132f4b0d26da6fe
Nia Nancheva <niaplnan.personal@gmail.com>
2021-10-30 18:41:19 +0300

    Create _6.java

---
+++ Project Euler/_6.java
@@ -0,0 +1,24 @@
+package proggy;
+
+import java.util.Scanner;
+
+public class _6 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        long sumSquares = 0;
+        long sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sumSquares = sumSquares + (i * i);
+            sum += i;
+        }
+
+        long squareSum = sum * sum;
+
+        long diff = squareSum - sumSquares;
+
+        System.out.println(diff);
+    }
+}
