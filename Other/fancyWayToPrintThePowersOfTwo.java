a17eec47ac6ed6d20764f8fd821feef8955a286f
Nia Nancheva <niaplnan.personal@gmail.com>
2021-11-02 15:42:04 +0200

    Update fancyWayToPrintThePowersOfTwo.java

--- Other/fancyWayToPrintThePowersOfTwo.java
+++ Other/fancyWayToPrintThePowersOfTwo.java
@@ -1,25 +1,23 @@
 import java.util.Scanner;
 
-public class fancyWayToPrintThePowersOfTwo {
-
+public class jhgfd {
     public static void main(String[] args) {
-      
-      // sorry for the long name of the class lol
+
+        //sorry for the long name of the class lol
 
         Scanner scan = new Scanner(System.in);
-        
+
         int n = Integer.parseInt(scan.nextLine());
         
-       // would probably crash if n > 30
-        
-        int x = 1;
-        int y = 0;
+        //would probably crash if n > 60
+
+        long x = 1;
+        long y = 0;
 
         for (int i = 0; i <= n; i++) {
             y = x + y;
             x = y;
             System.out.printf("%dth Power of 2: %d%n", i, x);
         }
-
     }
 }
